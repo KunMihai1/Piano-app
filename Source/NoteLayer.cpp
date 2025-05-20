cda5a250f12ddd8508815611fc8beb68a86bccb0
KunMihai1 <mihaikun2501@gmail.com>
2025-05-20 20:22:24 +0300

    Update

--- Source/NoteLayer.cpp
+++ Source/NoteLayer.cpp
@@ -8,7 +8,11 @@
   ==============================================================================
 */
 
-#include "NoteLayer.h"
+
+#include "NoteLayer.h" 
+#include <juce_opengl/juce_opengl.h>
+
+using namespace juce::gl;
 
 NoteLayer::NoteLayer(KeyboardUI& referenceKeyboard) : keyBoardUI{ referenceKeyboard }
 {
@@ -17,7 +21,6 @@ NoteLayer::NoteLayer(KeyboardUI& referenceKeyboard) : keyBoardUI{ referenceKeybo
 
     openGLContext.setRenderer(this);
     openGLContext.attachTo(*this);
-    openGLContext.setContinuousRepainting(true);
 }
 
 NoteLayer::~NoteLayer()
@@ -57,6 +60,10 @@ void NoteLayer::noteOnReceived(int midiNote)
 
                 activeNotes[midiNote] = newNote;
 
+                spawnParticlesForNote(midiNote);
+
+                if(!isTimerRunning())
+                    startTimerHz(60);
             }
         });
 }
@@ -102,16 +109,140 @@ void NoteLayer::newOpenGLContextCreated()
             gl_FragColor = vColour * alpha;
         }
     )FRAG";
+
+    shader = std::make_unique<juce::OpenGLShaderProgram>(openGLContext);
+    if (!shader->addVertexShader(vertexSource) || !shader->addFragmentShader(fragmentSource) || !shader->link())
+    {
+        jassertfalse;
+        shader.reset();
+        return;
+    }
+    positionAttr = new juce::OpenGLShaderProgram::Attribute(*shader, "position");
+    pointSizeAttr = new juce::OpenGLShaderProgram::Attribute(*shader, "pointSize");
+    colourAttr = new juce::OpenGLShaderProgram::Attribute(*shader, "colour");
+    openGLContext.extensions.glGenBuffers(1, &particleVBO);
 }
 
 void NoteLayer::renderOpenGL()
 {
-    
+    if (shader != nullptr && particleVBO != 0 && !particles.empty())
+    {
+        glClearColor(0.f, 0.f, 0.f, 0.f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        shader->use();
+
+        struct Vertex {
+            float x, y;
+            float size;
+            float r, g, b, a;
+        };
+
+        std::vector<Vertex> verts;
+        verts.reserve(particles.size());
+
+        for (const auto& p : particles)
+        {
+            verts.push_back({ p.pos.x, p.pos.y, p.size,
+                              p.colour.getFloatRed(),
+                              p.colour.getFloatGreen(),
+                              p.colour.getFloatBlue(),
+                              p.colour.getFloatAlpha() * p.life });
+        }
+
+        openGLContext.extensions.glBindBuffer(GL_ARRAY_BUFFER, particleVBO);
+        openGLContext.extensions.glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex)* verts.size(), verts.data(), GL_DYNAMIC_DRAW);
+
+        if (positionAttr != nullptr)
+        {
+            openGLContext.extensions.glVertexAttribPointer(positionAttr->attributeID, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, x));
+            openGLContext.extensions.glEnableVertexAttribArray(positionAttr->attributeID);
+        }
+        if (pointSizeAttr != nullptr)
+        {
+            openGLContext.extensions.glVertexAttribPointer(pointSizeAttr->attributeID, 1, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, size));
+            openGLContext.extensions.glEnableVertexAttribArray(pointSizeAttr->attributeID);
+        }
+        if (colourAttr != nullptr)
+        {
+            openGLContext.extensions.glVertexAttribPointer(colourAttr->attributeID, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, r));
+            openGLContext.extensions.glEnableVertexAttribArray(colourAttr->attributeID);
+        }
+
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE); // additive blend
+
+        glDrawArrays(GL_POINTS, 0, (GLsizei)verts.size());
+
+        // Cleanup
+        if (positionAttr) openGLContext.extensions.glDisableVertexAttribArray(positionAttr->attributeID);
+        if (pointSizeAttr) openGLContext.extensions.glDisableVertexAttribArray(pointSizeAttr->attributeID);
+        if (colourAttr) openGLContext.extensions.glDisableVertexAttribArray(colourAttr->attributeID);
+    }
 }
 
 void NoteLayer::openGLContextClosing()
 {
-    
+    if (particleVBO)
+    {
+        openGLContext.extensions.glDeleteBuffers(1, &particleVBO);
+        particleVBO = 0;
+    }
+    delete positionAttr;
+    positionAttr = nullptr;
+
+    delete pointSizeAttr;
+    pointSizeAttr = nullptr;
+
+    delete colourAttr;
+    colourAttr = nullptr;
+
+    shader.reset();
+}
+
+void NoteLayer::updateParticles()
+{
+    float dt = 1.0f / 60.0f;
+    for (auto& p : particles)
+    {
+        p.pos += p.velocity * dt;
+        p.life -= dt * 0.25f;
+    }
+
+
+    particles.erase(std::remove_if(particles.begin(), particles.end(),
+        [](const Particle& p)
+        {
+            return p.life <= 0.0f;
+        }),
+        particles.end());
+}
+
+void NoteLayer::spawnParticlesForNote(int midiNote)
+{
+    auto keyBounds = this->keyBoardUI.keys[midiNote].bounds;
+
+    // Convert pixel center to NDC [-1, 1]
+    float x_ndc = (keyBounds.getCentreX() / (float)getWidth()) * 2.0f - 1.0f;
+    float y_ndc = 1.0f - (keyBounds.getY() / (float)getHeight()) * 2.0f;
+
+    juce::Point<float> pos(x_ndc, y_ndc);
+
+    for (int i = 0; i < 10; i++)
+    {
+        Particle p;
+        p.pos = pos;
+
+        // Velocity should be small since NDC is from -1 to 1
+        p.velocity = {
+            (juce::Random::getSystemRandom().nextFloat() * 2.0f - 1.0f) * 0.5f,  // horizontal velocity in NDC units/sec
+            -(juce::Random::getSystemRandom().nextFloat() * 1.0f)                // vertical velocity in NDC units/sec
+        };
+
+        p.size = 10.0f + juce::Random::getSystemRandom().nextFloat() * 5.0f; // size in pixels
+        p.colour = juce::Colour::fromHSV(juce::Random::getSystemRandom().nextFloat(), 1.0f, 1.0f, 1.0f);
+        p.life = 1.0f;
+        particles.push_back(p);
+    }
 }
 
 
@@ -119,14 +250,14 @@ void NoteLayer::visibilityChanged()
 {
     if (!isVisible())
         stopTimer();
-    else startTimerHz(60);
+    //else startTimerHz(60);
 }
 
 void NoteLayer::timerCallback()
 {
     const float dt = 1.0f / 60.0f;
     const float riseSpeed = 350.0f;
-    const float fadeSpeed = 250.0f;
+    const float fadeSpeed = 350.0f;
     const float fadeTime = 5.0f;
     const float fadeRate = 1.0f / fadeTime;
 
@@ -154,5 +285,14 @@ void NoteLayer::timerCallback()
             it = fallingNotes.erase(it);
         else ++it;
     }
-    repaint();
+    updateParticles();
+
+    if (!particles.empty() || !activeNotes.empty() || !fallingNotes.empty())
+    {
+        openGLContext.triggerRepaint();
+        repaint();
+    }
+    else {
+        stopTimer();
+    }
 }
