a0981b53c4730341fc26a2664f7fa9ff99d64f50
KunMihai1 <mihaikun2501@gmail.com>
2025-05-20 15:19:29 +0300

    Updat

--- Source/NoteLayer.cpp
+++ Source/NoteLayer.cpp
@@ -14,11 +14,16 @@ NoteLayer::NoteLayer(KeyboardUI& referenceKeyboard) : keyBoardUI{ referenceKeybo
 {
     setSize(getWidth(), getHeight());
     keyBoardUI.midiHandler.addListener(this);
+
+    openGLContext.setRenderer(this);
+    openGLContext.attachTo(*this);
+    openGLContext.setContinuousRepainting(true);
 }
 
 NoteLayer::~NoteLayer()
 {
     keyBoardUI.midiHandler.removeListener(this);
+    openGLContext.detach();
 }
 
 void NoteLayer::paint(juce::Graphics& g)
@@ -26,12 +31,12 @@ void NoteLayer::paint(juce::Graphics& g)
     for (const auto& [midiNote, note] : activeNotes)
     {
         g.setColour(juce::Colours::green.withAlpha(0.8f));
-        g.fillRect(note.bounds);
+        g.fillRoundedRectangle(note.bounds.toFloat(), 6.0f);
     }
     for (const auto& note : fallingNotes)
     {
         g.setColour(juce::Colours::green.withAlpha(note.alpha));
-        g.fillRect(note.bounds);
+        g.fillRoundedRectangle(note.bounds.toFloat(), 6.0f);
     }
 }
 
@@ -41,12 +46,17 @@ void NoteLayer::noteOnReceived(int midiNote)
         {
             if (activeNotes.find(midiNote) == activeNotes.end())
             {
-
                 AnimatedNote newNote;
                 newNote.bounds = keyBoardUI.keys[midiNote].bounds;
+
+                newNote.height = 10.0f;
+                newNote.yPosition = static_cast<float>(getHeight()) - newNote.height;
+
+                newNote.bounds.setHeight(static_cast<int>(round(newNote.height)));
+                newNote.bounds.setY(static_cast<int>(round(newNote.yPosition)));
+
                 activeNotes[midiNote] = newNote;
-                activeNotes[midiNote].bounds.setHeight(10);
-                activeNotes[midiNote].bounds.setY(keyBoardUI.keys[midiNote].bounds.getY() - 12);
+
             }
         });
 }
@@ -65,6 +75,46 @@ void NoteLayer::noteOffReceived(int midiNote)
         });
 }
 
+void NoteLayer::newOpenGLContextCreated()
+{
+    const char* vertexSource = R"VERT(
+        attribute vec2 position;
+        attribute float pointSize;
+        attribute vec4 colour;
+        varying vec4 vColour;
+        void main()
+        {
+            gl_PointSize = pointSize;
+            gl_Position = vec4(position, 0.0, 1.0);
+            vColour = colour;
+        }
+    )VERT";
+
+    const char* fragmentSource = R"FRAG(
+        #ifdef GL_ES
+        precision mediump float;
+        #endif
+        varying vec4 vColour;
+        void main()
+        {
+            float dist = length(gl_PointCoord - vec2(0.5));
+            float alpha = smoothstep(0.5, 0.0, dist);
+            gl_FragColor = vColour * alpha;
+        }
+    )FRAG";
+}
+
+void NoteLayer::renderOpenGL()
+{
+    
+}
+
+void NoteLayer::openGLContextClosing()
+{
+    
+}
+
+
 void NoteLayer::visibilityChanged()
 {
     if (!isVisible())
@@ -75,8 +125,8 @@ void NoteLayer::visibilityChanged()
 void NoteLayer::timerCallback()
 {
     const float dt = 1.0f / 60.0f;
-    const float riseSpeed = 300.0f;
-    const float fadeSpeed = 150.0f;
+    const float riseSpeed = 350.0f;
+    const float fadeSpeed = 250.0f;
     const float fadeTime = 5.0f;
     const float fadeRate = 1.0f / fadeTime;
 
@@ -84,19 +134,21 @@ void NoteLayer::timerCallback()
     {
         AnimatedNote& n = pair.second;
 
-        int dy = static_cast<int>(round(riseSpeed * dt));
-        n.bounds.translate(0, -dy);
+        n.yPosition -= riseSpeed * dt;
+        n.height += riseSpeed * dt;
 
-        int dh = static_cast<int>(round(riseSpeed * dt));
-        int newH = n.bounds.getHeight() + dh;
-        n.bounds.setHeight(newH);
+        n.bounds.setY(static_cast<int>(round(n.yPosition)));
+        n.bounds.setHeight(static_cast<int>(round(n.height)));
     }
 
     for (auto it = fallingNotes.begin(); it != fallingNotes.end();)
     {
-        it->alpha -= dt * fadeRate;
-        int dy = static_cast<int>(round(fadeSpeed * dt));
-        it->bounds.translate(0, -dy);
+        AnimatedNote& n = *it;
+
+        n.alpha -= dt * fadeRate;
+
+        n.yPosition -= fadeSpeed * dt;
+        n.bounds.setY(static_cast<int>(round(n.yPosition)));
 
         if (it->alpha <= 0.0f)
             it = fallingNotes.erase(it);
--- Source/NoteLayer.h
+++ Source/NoteLayer.h
@@ -12,29 +12,53 @@
 
 #include "MidiHandler.h"
 #include "KeyboardUI.h"
+#include <juce_opengl/juce_opengl.h>
 
-
-class NoteLayer : public juce::Component, public MidiHandler::Listener, public juce::Timer
+class NoteLayer : public juce::Component, public MidiHandler::Listener, public juce::Timer, private juce::OpenGLRenderer
 {
 public:
     NoteLayer(KeyboardUI& referenceKeyboard);
     ~NoteLayer();
+    void visibilityChanged() override;
+    void timerCallback() override;
+
 
     void paint(juce::Graphics& g) override;
 
     void noteOnReceived(int midiNote) override;
     void noteOffReceived(int midiNote) override;
 
+    void newOpenGLContextCreated() override;
+    void renderOpenGL() override;
+    void openGLContextClosing() override;
+
 private:
+    struct Particle {
+        juce::Point<float> pos;
+        juce::Point<float> velocity;
+        float life = 1.0f;
+        float size = 6.0f;
+        juce::Colour colour;
+    };
+
     KeyboardUI& keyBoardUI;
 
-    void visibilityChanged() override;
-    void timerCallback() override;
+    juce::OpenGLContext openGLContext;
+    std::unique_ptr<juce::OpenGLShaderProgram> shader;
+    juce::OpenGLShaderProgram::Attribute* positionAttr = nullptr;
+    juce::OpenGLShaderProgram::Attribute* pointSizeAttr = nullptr;
+    juce::OpenGLShaderProgram::Attribute* colourAttr = nullptr;
+
+    juce::OpenGLBuffer vbo;
+    std::vector<Particle> particles;
+
 
     struct AnimatedNote {
         juce::Rectangle<int> bounds = { 0,0,0,0 };
+        float yPosition = 0.0f;
+        float height = 0.0f;
+
         float alpha = 1.0f;
-        float velocityY = -50.0f;
         bool isFalling = false;
     };
 
