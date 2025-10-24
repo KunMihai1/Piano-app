8da4622f67e618d36ce4185e70536dc357dd6dc7
KunMihai1 <mihaikun2501@gmail.com>
2025-10-24 15:13:57 +0300

    Update MidiHandlerAbstractSubject.h Specifications

--- Source/MidiHandlerAbstractSubject.h
+++ Source/MidiHandlerAbstractSubject.h
@@ -12,10 +12,37 @@
 
 #include <JuceHeader.h>
 
+/**
+ * @brief Interface for objects that want to listen to MIDI events from a MidiHandler.
+ * 
+ * Classes that implement MidiHandlerListener can receive notifications for
+ * MIDI note events and other incoming MIDI messages.
+ */
 class MidiHandlerListener {
 public:
     virtual ~MidiHandlerListener()=default;
+
+    /**
+     * @brief Called when a MIDI note-on event is received.
+     * @param midiNote The MIDI note number (0-127) that was pressed.
+     * 
+     * Override this method to respond to note-on events.
+     */
     virtual void noteOnReceived(int midiNote) { (void)midiNote; }
+
+    /**
+     * @brief Called when a MIDI note-off event is received.
+     * @param midiNote The MIDI note number (0-127) that was released.
+     * 
+     * Override this method to respond to note-off events.
+     */
     virtual void noteOffReceived(int midiNote) { (void)midiNote; }
+
+    /**
+     * @brief Called for all incoming MIDI messages.
+     * @param message The full juce::MidiMessage received.
+     * 
+     * Override this method to handle any kind of MIDI message.
+     */
     virtual void handleIncomingMessage(const juce::MidiMessage& message) { (void)message; }
 };
