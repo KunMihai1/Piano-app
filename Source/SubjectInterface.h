d6d9e8008c3e064ccb6e40e4d954b224298d2460
KunMihai1 <mihaikun2501@gmail.com>
2025-10-24 15:36:46 +0300

    Update SubjectInterface.h Specifications

--- Source/SubjectInterface.h
+++ Source/SubjectInterface.h
@@ -13,18 +13,45 @@
 #include "trackListComponentListener.h"
 #include "TrackPlayerListener.h"
 
+
+/**
+ * @class Subject
+ * @brief Template class implementing the Observer pattern for notifying listeners.
+ *
+ * This class maintains a list of listeners and provides methods to add, remove,
+ * and notify them about various events. It is designed to work with different types
+ * of listeners that implement the required callback interface.
+ *
+ * @tparam ListenerType Type of the listener class.
+ */
 template <typename ListenerType>
 class Subject
 {
 public:
+    /**
+     * @brief Adds a listener to the subject.
+     * @param listener Pointer to the listener to be added.
+     */
     void addListener(ListenerType* listener)
     {
         listeners.add(listener);
     }
+
+    /**
+     * @brief Removes a listener from the subject.
+     * @param listener Pointer to the listener to be removed.
+     */
     void removeListener(ListenerType* listener)
     {
         listeners.remove(listener);
     }
+
+    /**
+     * @brief Notifies all listeners about a playback settings update.
+     * @param channel MIDI channel affected.
+     * @param newVolume New volume value.
+     * @param newInstrument New instrument/program number.
+     */
     void notify(int channel, int newVolume, int newInstrument)
     {
         listeners.call([=](ListenerType& listener)
@@ -33,6 +60,9 @@ public:
             });
     }
 
+    /**
+     * @brief Notifies all listeners that a track is being added and requires normalization.
+     */
     void notifyAddingToTrackList()
     {
         listeners.call([=](ListenerType& listener)
@@ -41,6 +71,10 @@ public:
             });
     }
 
+    /**
+     * @brief Notifies all listeners of the current playback time.
+     * @param currentTime Current playback time in seconds.
+     */
     void notifyMidPlay(double currentTime)
     {
         listeners.call([=](ListenerType& listener)
