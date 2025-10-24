7e5d68161c81e6aab0ca1e67fdd7d35ec6eba913
KunMihai1 <mihaikun2501@gmail.com>
2025-10-24 15:32:26 +0300

    Update SelectableLabel.h Specifications

--- Source/SelectableLabel.h
+++ Source/SelectableLabel.h
@@ -11,11 +11,31 @@
 #pragma once
 #include <JuceHeader.h>
 
+/**
+ * @class SelectableLabel
+ * @brief A JUCE Label that supports click callbacks.
+ * 
+ * This class extends juce::Label to provide a simple `onClick` callback 
+ * when the label is clicked. It can be used in UI components where 
+ * labels need to act as interactive elements.
+ */
 class SelectableLabel : public juce::Label
 {
 public:
+    /**
+     * @brief Callback invoked when the label is clicked.
+     * 
+     * Set this function to handle mouse clicks on the label.
+     */
     std::function<void()> onClick;
 
+    /**
+     * @brief Handles mouse down events.
+     * @param event The mouse event that triggered this callback.
+     * 
+     * If the `onClick` callback is set, it is called first, then the base 
+     * juce::Label mouseDown is invoked.
+     */
     void mouseDown(const juce::MouseEvent& event) override;
 
 
