92ed1d13eca39867d1f6a4f499de25b9e60b9dfa
KunMihai1 <mihaikun2501@gmail.com>
2025-10-24 15:39:05 +0300

    Update TrackPlayerListener.h Specifications

--- Source/TrackPlayerListener.h
+++ Source/TrackPlayerListener.h
@@ -10,11 +10,31 @@
 
 #pragma once
 
+/**
+ * @file TrackPlayerListener.h
+ * @brief Interface for objects that want to listen to track playback updates.
+ * 
+ * This interface allows implementing classes to receive updates on the current
+ * playback position of a track, enabling synchronized UI updates or other logic.
+ */
+
+
+/**
+ * @class TrackPlayerListener
+ * @brief Listener interface for track playback events.
+ */
 class TrackPlayerListener
 {
 public:
-    
+
+    /**
+     * @brief Called to update the current row or position based on playback time.
+     * @param currentTime Current playback time in seconds.
+     */
     virtual void updateCurrentRowBasedOnTime(double currentTime) = 0;
 
+    /**
+     * @brief Virtual destructor.
+     */
     virtual ~TrackPlayerListener() = default;
 };
