093a342a3f18c2038c6e2d5ff4d4a1ff8cf1c0e0
KunMihai1 <mihaikun2501@gmail.com>
2025-10-29 21:45:35 +0200

    Update displayGUI.h

--- Source/displayGUI.h
+++ Source/displayGUI.h
@@ -790,22 +790,56 @@ private:
     JUCE_LEAK_DETECTOR(StylesListComponent)
 };
 
+
+/**
+ * @class MyTabbedComponent
+ * @brief Custom tabbed component that allows a callback when the current tab changes.
+ *
+ * Extends `juce::TabbedComponent` and adds a std::function callback
+ * `onTabChanged` which is called whenever the current tab is changed.
+ */
 class MyTabbedComponent : public juce::TabbedComponent
 {
 public:
+
+    /**
+     * @brief Constructs a MyTabbedComponent with a specified tab orientation.
+     * @param orientation Orientation of the tab bar (e.g., TabsAtTop, TabsAtLeft)
+     */
     MyTabbedComponent(juce::TabbedButtonBar::Orientation orientation)
         : juce::TabbedComponent(orientation) {}
 
+
+     /**
+     * @brief Called by JUCE when the current tab changes.
+     * @param newCurrentTabIndex Index of the newly selected tab
+     * @param newTabName Name of the newly selected tab
+     */
     void currentTabChanged(int newCurrentTabIndex, const juce::String& newTabName) override;
 
+    /** @brief Callback function triggered whenever the current tab changes */
     std::function<void(int, juce::String)> onTabChanged;
 
     //JUCE_LEAK_DETECTOR(MyTabbedComponent)
 };
 
+/**
+ * @class DisplayListener
+ * @brief Abstract interface to receive updates from the Display component.
+ *
+ * Classes implementing `DisplayListener` can receive notifications when
+ * playback settings are changed.
+ */
 class DisplayListener {
 public:
+
+    /** @brief Destructor */
     virtual ~DisplayListener() = default;
+
+    /**
+     * @brief Called when playback settings change.
+     * @param settings The new playback settings
+     */
     virtual void playBackSettingsChanged(const PlayBackSettings& settings)=0;
 };
 
