38f1fcdc8f6e6828f6636bcd7e5e8b7fe8028d81
KunMihai1 <mihaikun2501@gmail.com>
2025-12-03 02:25:32 +0200

    Update sections button gui
    done with the clicking and unclicking logic for now

--- Source/SectionsComponent.h
+++ Source/SectionsComponent.h
@@ -16,6 +16,9 @@
 class StyleSectionComponent: public juce::Component
 {
 public:
+    std::function<void()> undoLastClickedForOtherGroupSections;
+
+
     StyleSectionComponent(const std::vector<juce::String>& names, const std::vector<std::vector<juce::String>>& groupNames,
         const std::unordered_map<juce::String, std::function<void()>>& buttonCallbacks);
 
@@ -23,9 +26,16 @@ public:
 
     void applyChangeColour(juce::TextButton& button, bool activated = false);
 
+    juce::TextButton* getLastUsedButton();
+
+    void deactivateLastClicked();
+
 private:
     juce::OwnedArray<SectionGroupComponent> sectionGroups;
     const std::unordered_map<juce::String, std::function<void()>>& callbacks;
+    juce::TextButton* lastClickedButton=nullptr;
+    std::unordered_map<juce::String, bool>* lastActivationMap=nullptr;
+    bool isProgramaticClick = false;
 
     void assignCallBacks();
 };
